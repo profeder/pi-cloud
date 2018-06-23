84e300554fdd934b7b30cc2e90ce57625ae0d2f2
Federico Profeti <federico.profeti@gmail.com>
2018-06-23 17:57:25 +0200

    Inserito script installazione

--- cli/docker_install.sh
+++ cli/docker_install.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/env bash
 
-echo 'Aggiornamento pacchetti in corso...'
+echo 'Upgrading package lists...'
 sudo apt-get update > /dev/null
-
-echo 'Ricerca componenti installati...'
-compose="$(dpkg -l | grep docker-compose)"
-echo $compose
-if [ ! -z  $compose ]
-then
-    echo 'docker compose non installato'
-    #apt-get install -y docker-compose
-fi
+sudo apt-get install -y docker.io docker docker-compose > /dev/null
