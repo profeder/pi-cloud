ffa8ee48eb13107a72e077bc18b6ebf044bf414b
Federico Profeti <federico.profeti@gmail.com>
2018-06-24 14:16:08 +0200

    add permission to menu

--- webadmin/main/templatetags/menu.py
+++ webadmin/main/templatetags/menu.py
@@ -1,16 +1,20 @@
-from ..models import MenuItem
+from ..models import MenuItem, Permission
 from django import template
+from django.contrib.auth.models import AnonymousUser
+from django.contrib.auth.context_processors import PermWrapper
 
 
 register = template.Library()
 
-
 @register.simple_tag(name='menu')
-def getBuildMenu():
-    models=  MenuItem.objects.order_by('super_item_id', 'order')
+def getBuildMenu(user, perms):
     items = []
-    for item in models:
-        if item.super_item_id is None:
-            items.append(item)
-
+    if user.username != AnonymousUser.username:
+        perm = Permission.objects.filter(user=user)
+        models = MenuItem.objects.order_by('super_item_id', 'order')
+        for item in models:
+            if item.super_item_id is None:
+                print(user.has_perm(item.permission.codename))
+                if user.has_perm(item.permission.codename):
+                    items.append(item)
     return items
